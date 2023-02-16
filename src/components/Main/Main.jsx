ea25e2e0847aa8e90c81b64a9c3a7ac973cbc881
Mehmet Akif Karaöz <karaozmehmetakif@gmail.com>
2023-02-17 02:06:11 +0300

    finished

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,11 +3,11 @@ import { data } from "../../helpers/data";
 import Card from "./Card";
 
 const Main = () => {
-  console.log(data);
+  //   console.log(data);
   return (
     <div className="card-container">
       {data.map((item) => {
-        return <Card />;
+        return <Card {...item} />;
       })}
     </div>
   );
